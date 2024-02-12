16f391dd256a4eddc5ceb7e804c9e7c311b1b08d
Willem Dalton <whd1964@rit.edu>
2024-02-12 10:59:44 -0500

    started the player physics

---
+++ RopePhysics/Player.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RopePhysics
+{
+    internal struct Player
+    {
+        private Texture2D asset;
+
+        private Vector2 position;
+        private Vector2 velocity;
+        private Vector2 direction;
+
+        private int speed;
+        private const int maxSpeed = 10;
+
+        private const int acceleration = 1;
+        private const int friction = 1;
+
+        public Player(Vector2 position, Texture2D asset)
+        {
+            this.position = position;
+            this.asset = asset;
+
+            // watch out for this, may only be referencing not copying
+            velocity = Vector2.Zero;
+            direction = Vector2.Zero;
+
+            speed = 0;
+        }
+
+        
+        
+        
+    }
+}
---
+++ RopePhysics/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new RopePhysics.Game1();
+game.Run();
